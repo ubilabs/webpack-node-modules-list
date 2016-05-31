ae167c6656a3636065fe492db978f9a1e746c910
Keno Schwalb <keno@kenoschwalb.com>
2016-05-31 16:58:12 +0200

    Only list node modules & allow setting a custom chunk name

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
+# Node Modules List Webpack Plugin
+
 [![npm version](https://badge.fury.io/js/webpack-node-modules-list.svg)](https://badge.fury.io/js/webpack-node-modules-list)
 
-Exports meta data of all used node modules of a webpack bundle to a file.
+Exports metadata of all used node modules of a webpack bundle to a file.  
+This plugin works perfectly with the `SplitByPathPlugin` plugin.
 
-Currently all included node modules of the webpack chunck `vendor` will be collected and the name, version, license and link to the repo will be written to a `npm-modules.md` file in the webpack output dir. This plugin works perfectly with `SplitByPathPlugin` plugin.
+## About
+
+This plugin creates a list with the name, version, license, and link to the repo of each node module in the webpack bundle.  
+The output file is currently hardcoded and set to `npm-modules.md` in the webpack output directory.
+
+## Usage
+
+Simply include this plugin in your webpack configuration file.
 
 ```
 const ExportNodeModules = require('webpack-node-modules-list'),
@@ -10,12 +20,26 @@ const ExportNodeModules = require('webpack-node-modules-list'),
 
 module.exports = {
 plugins: [
+  new ExportNodeModules()
+]}
+```
+
+Alternatively, it is possible to pass a chunk name to the constructor.  
+This allows to filter the output so that it only includes chunks with a matching chunk name.
+
+```
+const ExportNodeModules = require('webpack-node-modules-list'),
+  SplitByPathPlugin = require('webpack-split-by-path'),
+  chunkName = 'vendor';
+
+module.exports = {
+plugins: [
   new SplitByPathPlugin([
     {
-      name: 'vendor',
+      name: chunkName,
       path: path.join(__dirname, 'node_modules')
     }
   ]),
-  new ExportNodeModules()
+  new ExportNodeModules({chunkName})
 ]}
 ```
--- index.js
+++ index.js
@@ -1,39 +1,45 @@
 'use strict';
 
 const path = require('path'),
-  fs = require('fs'),
-  npmModules = new Map();
+  fs = require('fs');
 
-function ExportNodeModules() {}
+function ExportNodeModules(options) {
+  options = options || {};
+
+  this.chunkName = options.chunkName;
+}
 
 ExportNodeModules.prototype.apply = function(compiler) {
-  compiler.plugin('emit', function(compilation, callback) {
-    let npmModulesList = '';
+  compiler.plugin('emit', (compilation, callback) => {
+    let npmModulesList = '',
+      npmModules = new Map();
 
-    compilation.chunks.forEach(function(chunk) {
-      if (chunk.name === 'vendor') {
-        chunk.modules.forEach(function(module) {
-          const contextArray = module.context.split('/');
-
-          contextArray.splice(contextArray.indexOf('node_modules') + 2);
-
-          let packageJson = {},
-            context = contextArray.join('/'),
-            npmModule = contextArray[contextArray.indexOf('node_modules') + 1],
-            packageJsonFile = path.join(context, 'package.json');
-
-          if (fs.existsSync(packageJsonFile)) {
-            packageJson = JSON.parse(fs.readFileSync(packageJsonFile, 'UTF-8'));
-          }
-
-          npmModules.set(packageJson.name, {
-            name: packageJson.name,
-            version: packageJson.version,
-            homepage: packageJson.homepage,
-            license: getLicenses(packageJson)
-          });
-        });
+    compilation.chunks.forEach(chunk => {
+      if(this.chunkName && this.chunkName !== chunk.name) {
+        return;
       }
+
+      // exclude anything that isn't a node module
+      let modules = chunk.modules.filter(module =>
+        module.context.indexOf('node_modules') !== -1);
+
+      modules.forEach(function(module) {
+        const contextArray = module.context.split('/');
+
+        contextArray.splice(contextArray.indexOf('node_modules') + 2);
+
+        let context = contextArray.join('/'),
+          npmModule = contextArray[contextArray.indexOf('node_modules') + 1],
+          packageJsonFile = path.join(context, 'package.json'),
+          packageJson = JSON.parse(fs.readFileSync(packageJsonFile, 'UTF-8'));
+
+        npmModules.set(packageJson.name, {
+          name: packageJson.name,
+          version: packageJson.version,
+          homepage: packageJson.homepage,
+          license: getLicenses(packageJson)
+        });
+      });
     });
 
     Array.from(npmModules.keys())
@@ -41,7 +47,7 @@ ExportNodeModules.prototype.apply = function(compiler) {
       .map(key => npmModules.get(key))
       .forEach(module => {
         npmModulesList += `[${module.name}@${module.version}: ` +
-          `${module.license}](${module.homepage})\n`;
+          `${module.license}](${module.homepage})  \n`;
       });
 
     // Insert this list into the Webpack build as a new file asset:
